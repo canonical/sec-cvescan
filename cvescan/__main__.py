d4c37feaf48114772dffd5bcc5d62bb86fa58458
Mike Salvatore <mike.salvatore@canonical.com>
2020-08-14 08:37:09 -0400

    rearrange functions in__main__.py to have a top-down flow

--- cvescan/__main__.py
+++ cvescan/__main__.py
@@ -31,34 +31,6 @@ from cvescan.target_sysinfo import TargetSysInfo
 from .version import get_version
 
 
-def set_output_verbosity(opt):
-    if opt.silent:
-        spin.silent = True
-        return get_null_logger()
-
-    logger = logging.getLogger(const.STDOUT_LOGGER_NAME)
-
-    if opt.verbose:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-
-    log_formatter = logging.Formatter("%(message)s")
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(log_formatter)
-    logger.addHandler(stream_handler)
-
-    return logger
-
-
-def get_null_logger():
-    logger = logging.getLogger(const.NULL_LOGGER_NAME)
-    if not logger.hasHandlers():
-        logger.addHandler(logging.NullHandler())
-
-    return logger
-
-
 def error_exit(msg, code=None):
     if code is None:
         code = error_exit.default_code
@@ -68,6 +40,50 @@ def error_exit(msg, code=None):
 
 
 error_exit.default_code = const.ERROR_RETURN_CODE
+
+
+def main():
+    args = parse_args()
+
+    try:
+        opt = Options(args)
+    except (ArgumentError, ValueError) as err:
+        error_exit(f"Invalid option or argument -- {err}", const.CLI_ERROR_RETURN_CODE)
+
+    error_exit.default_code = (
+        const.NAGIOS_UNKNOWN_RETURN_CODE if opt.nagios_mode else const.ERROR_RETURN_CODE
+    )
+
+    logger = set_output_verbosity(opt)
+    try:
+        local_sysinfo, target_sysinfo = get_sysinfo(opt, logger)
+    except (FileNotFoundError, PermissionError) as err:
+        error_exit(f"Failed to determine the correct Ubuntu codename -- {err}")
+    except DistribIDError as di:
+        error_exit(
+            f"Invalid linux distribution detected, CVEScan must be run on Ubuntu -- {di}"
+        )
+    except PkgCountError as pke:
+        error_exit(f"Failed to determine the local package count -- {pke}")
+
+    download_cache = USTDownloadCache(logger)
+    uct_data = load_uct_data(opt, download_cache, target_sysinfo)
+
+    scan_results = run_scan(target_sysinfo, uct_data, logger)
+
+    output_formatter = load_output_formatter(opt, logger)
+    (formatted_output, return_code) = output_formatter.format_output(
+        scan_results, target_sysinfo
+    )
+
+    try:
+        output_logger = get_output_logger(opt, logger)
+        output(output_logger, formatted_output, return_code)
+        sys.exit(return_code)
+    except socket.gaierror as se:
+        error_exit(
+            f"Failed to send syslog output to {opt.syslog_host}:{opt.syslog_port} -- {se}"
+        )
 
 
 def parse_args():
@@ -124,33 +140,43 @@ def parse_args():
     return cvescan_ap.parse_args()
 
 
-def load_output_formatter(opt, logger):
-    sorter = load_output_sorter(opt)
+def set_output_verbosity(opt):
+    if opt.silent:
+        spin.silent = True
+        return get_null_logger()
 
-    if opt.csv:
-        return CSVOutputFormatter(opt, logger, sorter=sorter)
+    logger = logging.getLogger(const.STDOUT_LOGGER_NAME)
 
-    if opt.cve:
-        return CVEOutputFormatter(opt, logger)
+    if opt.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
-    if opt.json:
-        return JSONOutputFormatter(opt, logger, sorter=sorter, indent=4)
+    log_formatter = logging.Formatter("%(message)s")
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(log_formatter)
+    logger.addHandler(stream_handler)
 
-    if opt.nagios_mode:
-        return NagiosOutputFormatter(opt, logger, sorter=sorter)
-
-    if opt.syslog or opt.syslog_light:
-        json_output_formatter = JSONOutputFormatter(
-            opt, logger, sorter=sorter, indent=None
-        )
-        return SyslogOutputFormatter(opt, logger, json_output_formatter)
-
-    return CLIOutputFormatter(opt, logger, sorter=sorter)
+    return logger
 
 
-def load_output_sorter(opt):
-    pkg_sorter = PackageScanResultSorter()
-    return CVEScanResultSorter(subsorters=[pkg_sorter])
+def get_null_logger():
+    logger = logging.getLogger(const.NULL_LOGGER_NAME)
+    if not logger.hasHandlers():
+        logger.addHandler(logging.NullHandler())
+
+    return logger
+
+
+def get_sysinfo(opt, logger):
+    local_sysinfo = LocalSysInfo(logger)
+    target_sysinfo = TargetSysInfo(opt, local_sysinfo)
+
+    debug.log_config_options(opt, logger)
+    debug.log_local_system_info(local_sysinfo, opt.manifest_mode, logger)
+    debug.log_target_system_info(target_sysinfo, logger)
+
+    return local_sysinfo, target_sysinfo
 
 
 def spin(start_text, ok, fail):
@@ -207,66 +233,33 @@ def run_scan(target_sysinfo, uct_data, logger):
     )
 
 
-def main():
-    args = parse_args()
+def load_output_formatter(opt, logger):
+    sorter = load_output_sorter(opt)
 
-    try:
-        opt = Options(args)
-    except (ArgumentError, ValueError) as err:
-        error_exit(f"Invalid option or argument -- {err}", const.CLI_ERROR_RETURN_CODE)
+    if opt.csv:
+        return CSVOutputFormatter(opt, logger, sorter=sorter)
 
-    error_exit.default_code = (
-        const.NAGIOS_UNKNOWN_RETURN_CODE if opt.nagios_mode else const.ERROR_RETURN_CODE
-    )
+    if opt.cve:
+        return CVEOutputFormatter(opt, logger)
 
-    logger = set_output_verbosity(opt)
-    try:
-        local_sysinfo, target_sysinfo = get_sysinfo(opt, logger)
-    except (FileNotFoundError, PermissionError) as err:
-        error_exit(f"Failed to determine the correct Ubuntu codename -- {err}")
-    except DistribIDError as di:
-        error_exit(
-            f"Invalid linux distribution detected, CVEScan must be run on Ubuntu -- {di}"
+    if opt.json:
+        return JSONOutputFormatter(opt, logger, sorter=sorter, indent=4)
+
+    if opt.nagios_mode:
+        return NagiosOutputFormatter(opt, logger, sorter=sorter)
+
+    if opt.syslog or opt.syslog_light:
+        json_output_formatter = JSONOutputFormatter(
+            opt, logger, sorter=sorter, indent=None
         )
-    except PkgCountError as pke:
-        error_exit(f"Failed to determine the local package count -- {pke}")
+        return SyslogOutputFormatter(opt, logger, json_output_formatter)
 
-    download_cache = USTDownloadCache(logger)
-    uct_data = load_uct_data(opt, download_cache, target_sysinfo)
-
-    scan_results = run_scan(target_sysinfo, uct_data, logger)
-
-    output_formatter = load_output_formatter(opt, logger)
-    (formatted_output, return_code) = output_formatter.format_output(
-        scan_results, target_sysinfo
-    )
-
-    try:
-        output_logger = get_output_logger(opt, logger)
-        output(output_logger, formatted_output, return_code)
-        sys.exit(return_code)
-    except socket.gaierror as se:
-        error_exit(
-            f"Failed to send syslog output to {opt.syslog_host}:{opt.syslog_port} -- {se}"
-        )
+    return CLIOutputFormatter(opt, logger, sorter=sorter)
 
 
-def get_sysinfo(opt, logger):
-    local_sysinfo = LocalSysInfo(logger)
-    target_sysinfo = TargetSysInfo(opt, local_sysinfo)
-
-    debug.log_config_options(opt, logger)
-    debug.log_local_system_info(local_sysinfo, opt.manifest_mode, logger)
-    debug.log_target_system_info(target_sysinfo, logger)
-
-    return local_sysinfo, target_sysinfo
-
-
-def output(output_logger, formatted_output, return_code):
-    if return_code == const.SUCCESS_RETURN_CODE:
-        output_logger.info(formatted_output)
-    else:
-        output_logger.warning(formatted_output)
+def load_output_sorter(opt):
+    pkg_sorter = PackageScanResultSorter()
+    return CVEScanResultSorter(subsorters=[pkg_sorter])
 
 
 def get_output_logger(opt, logger):
@@ -299,6 +292,13 @@ def get_syslog_logger(host, port):
     syslog_logger.setLevel(logging.INFO)
 
     return syslog_logger
+
+
+def output(output_logger, formatted_output, return_code):
+    if return_code == const.SUCCESS_RETURN_CODE:
+        output_logger.info(formatted_output)
+    else:
+        output_logger.warning(formatted_output)
 
 
 if __name__ == "__main__":
